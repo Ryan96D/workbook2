b53b92141cd0519dd07f0c2c314c0c7ae411ba66
Ryan <doryan6996@gmail.com>
2025-04-20 20:58:32 -0700

    Cellphone exercise

--- CellPhoneService/src/main/java/CellPhone.java
+++ CellPhoneService/src/main/java/CellPhone.java
@@ -3,6 +3,54 @@ public class CellPhone {
     int serial;
     String model;
     String carrier;
-    int phoneNumber;
+    String phoneNumber;
     String ownerName;
+
+    CellPhone (){
+        serial = 0;
+        model = "";
+        carrier = "";
+        phoneNumber = "";
+        ownerName = "";
+    }
+
+    public int getSerial() {
+        return serial;
+    }
+
+    public void setSerial(int serial) {
+        this.serial = serial;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
 }
--- CellPhoneService/src/main/java/CellPhoneApplication.java
+++ CellPhoneService/src/main/java/CellPhoneApplication.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        Scanner scanny = new Scanner(System.in);
+        CellPhone myPhone = new CellPhone();
+
+        System.out.println("What is your phone's serial number?");
+        myPhone.setSerial(scanny.nextInt());
+        scanny.nextLine(); // eats leftover Enter from nextInt
+
+        System.out.println("What is your phone model?");
+        myPhone.setModel(scanny.nextLine());
+
+        System.out.println("What is your phone carrier?");
+        myPhone.setCarrier(scanny.nextLine());
+
+        System.out.println("What is your phone number?");
+        myPhone.setPhoneNumber(scanny.nextLine());
+
+        System.out.println("Who is the phone owner's name?");
+        myPhone.setOwnerName(scanny.nextLine());
+
+        System.out.println("Serial number: "+myPhone.getSerial());
+        System.out.println("Phone model: "+myPhone.getModel());
+        System.out.println("Phone carrier: "+myPhone.getCarrier());
+        System.out.println("Phone number: "+myPhone.getPhoneNumber());
+        System.out.println("Phone owner: "+myPhone.getOwnerName());
+
+
+
+
 
     }
 }
